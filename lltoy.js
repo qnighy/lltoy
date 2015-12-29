5c8aeee8e57f504a864e29175709b9b5ae625eeb
Masaki Hara <ackie.h.gmai@gmail.com>
2015-12-29 16:10:06 +0900

    add logic selector.

--- lltoy.js
+++ lltoy.js
@@ -1,22 +1,54 @@
 "use strict";
 
 $(function() {
-  var default_propositions = $("#default-propositions");
   var proposition_to_solve = $("#proposition-to-solve");
   var proof_status = $("#proof-status");
   var proof_status_label = $("<span class=\"label label-default\"></span>").appendTo(proof_status);
   proof_status_label.text("Input or select a proposition.");
   var proof_area = $("#proof-area");
-  for(var i = 0; i < default_propositions_data.length; ++i) {
-    var li = $("<li></li>");
-    var a = $("<a></a>").appendTo(li);
-    a.attr("href", "javascript:void(0)");
-    a.text(default_propositions_data[i]);
-    a.click(function() {
-      proposition_to_solve.val($(this).text());
-    });
-    default_propositions.append(li);
-  }
+
+  var selected_logic = null;
+  var logic_short_names = {
+    "classical" : "Cl",
+    "intuitionistic" : "Int",
+    "modal_k" : "K",
+    "modal_t" : "T",
+    "modal_s4" : "S4",
+    "linear" : "LL",
+  };
+  var update_selected_logic = function(new_selected_logic) {
+    selected_logic = new_selected_logic;
+    $("#select_classical_logic").removeClass("btn-default btn-primary").addClass(selected_logic == "classical" ? "btn-primary" : "btn-default");
+    $("#select_intuitionistic_logic").removeClass("btn-default btn-primary").addClass(selected_logic == "intuitionistic" ? "btn-primary" : "btn-default");
+    $("#select_modal_logic").removeClass("btn-default btn-primary").addClass(selected_logic.match(/^modal_/) ? "btn-primary" : "btn-default");
+    $("#select_modal_logic_k").removeClass("active").addClass(selected_logic == "modal_k" ? "active" : "");
+    $("#select_modal_logic_t").removeClass("active").addClass(selected_logic == "modal_t" ? "active" : "");
+    $("#select_modal_logic_s4").removeClass("active").addClass(selected_logic == "modal_s4" ? "active" : "");
+    $("#select_linear_logic").removeClass("btn-default btn-primary").addClass(selected_logic == "linear" ? "btn-primary" : "btn-default");
+
+    $("#heading_title").text("LL Toy (" + logic_short_names[selected_logic] + ")");
+
+    var default_propositions = $("#default-propositions");
+    default_propositions.empty();
+    for(var i = 0; i < default_propositions_data[selected_logic].length; ++i) {
+      var li = $("<li></li>");
+      var a = $("<a></a>").appendTo(li);
+      a.attr("href", "javascript:void(0)");
+      a.text(default_propositions_data[selected_logic][i]);
+      a.click(function() {
+        proposition_to_solve.val($(this).text());
+      });
+      default_propositions.append(li);
+    }
+    proposition_to_solve.val(default_propositions_data[selected_logic][0]);
+  };
+  $("#select_classical_logic").click(function() { update_selected_logic("classical"); });
+  $("#select_intuitionistic_logic").click(function() { update_selected_logic("intuitionistic"); });
+  $("#select_modal_logic_k a").click(function() { update_selected_logic("modal_k"); });
+  $("#select_modal_logic_t a").click(function() { update_selected_logic("modal_t"); });
+  $("#select_modal_logic_s4 a").click(function() { update_selected_logic("modal_s4"); });
+  $("#select_linear_logic").click(function() { update_selected_logic("linear"); });
+  update_selected_logic("classical");
   var root_sequent = null;
   $("#start-proving").click(function() {
     var propstr = proposition_to_solve.val();
